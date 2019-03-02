9babf2eb669caf4a45cd32288dfcfb4614a15116
Peter Xie <515971172@qq.com>
2019-03-02 22:49:39 +0800

    commit

---
+++ Assets/Scripts/FoodType.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodType
+{
+    public FoodType(int main, int sub)
+    {
+        if( main <=99&& sub <= 99)
+        {
+            this.mainType = main;
+            this.subType = sub;
+        }
+        
+    }
+
+    public string GetMain()
+    {
+        string str = "" + mainType + subType;
+        return str;
+    }
+
+    public string GetSub()
+    {
+        string str = "" + mainType + "00";
+        return str;
+    }
+
+    private int mainType;
+
+    private int subType;
+}
+
---
+++ Assets/Scripts/GamePersist.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePersist
+{
+    static private GamePersist gamePersist;
+
+    static public GamePersist Get()
+    {
+        if (gamePersist == null)
+            gamePersist = new GamePersist();
+        return gamePersist;
+    }
+
+    GamePersist()
+    {
+    }
+
+    private Dictionary<string, string> foodMap;
+
+    private void Init()
+    {
+        // 初始化food map
+        // 从xml或者json加载
+    }
+
+    public bool InsertRecord(string input, string result)
+    {
+        foodMap.Add(input, result);
+        return true;
+    }
+
+    public string GetResult(string input)
+    {
+        return foodMap[input];
+    }
+}
