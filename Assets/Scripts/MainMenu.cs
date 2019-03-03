c3e939ddf2cd0b044d2c6ce6239407ab618af0b3
tuxingc <317254085@qq.com>
2019-03-03 15:07:08 +0800

    完成了主菜单的制作

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void CompoundSystem()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void EditSystem()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
